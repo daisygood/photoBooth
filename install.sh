c9a75fa44cff8c5ff37774fdac587868dea73fb7
Daisy Good <daisy.m.good@gmail.com>
2018-09-11 21:11:18 -0500

    add folder

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ sudo python3 setup.py install
 cd ../gui/
 sudo python3 setup.py install
 cd ..
+echo "Installing graphicsmagick"
 sudo apt-get install graphicsmagick
+echo "Check if pics folder exists"
+mkdir -p pics
