bcbfbdcd65b8d870740ff0e1820975c74c9c3b03
goo7387 <daisygood@northwesternmutual.com>
2019-03-13 07:25:54 -0500

    add overlay for upaf event

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ replay_cycles = 2 # how many times to show each photo on-screen after taking
 ####################
 real_path = os.path.dirname(os.path.realpath(__file__))
 file_path = real_path + '/pics/'
-overlay_path = real_path + '/graphics/grace-hopper-overlay.png'
+overlay_path = real_path + '/graphics/upaf-overlay.png'
 
 # initialize pygame
 pygame.init()
@@ -183,23 +183,23 @@ def taking_pics():
 # Covert image to gif
 def convert():
   
-  # overlayname = file_path + now + '-0'+  str(x) + '-overlay.jpg'
-  # addOverlayCmd = 'gm composite -geometry +0+1513 -compose Over ' + overlay_path + ' ' + file_path + now + "-0" +str(x) + ".jpg" + ' ' + ' ' + overlayname
-  # os.system(addOverlayCmd)
-  # graphicsmagick = "gm convert -size 1500x1500 " + file_path + now + "-0" + str(x) + "-overlay.jpg -thumbnail 1500x1500 " + file_path + now + "-0" + str(x) + "-sm.jpg"
-  # os.system(graphicsmagick)
-  # global gif_file_name
-  # gif_file_name = file_path + now + str(random.randint(1,1000000000000000))
-  # graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + file_path + now + "*-sm.jpg " + gif_file_name + ".gif"
+  for x in range(1, total_pics+1): #batch process all the images
+    overlayname = file_path + now + '-0'+  str(x) + '-overlay.jpg'
+    addOverlayCmd = 'gm composite -geometry +0+792 -compose Over ' + overlay_path + ' ' + file_path + now + "-0" +str(x) + ".jpg" + ' ' + ' ' + overlayname
+    os.system(addOverlayCmd)
 
-  # os.system(graphicsmagick)
-
-  # Without Overlay
   global gif_file_name
   gif_file_name = file_path + now + str(random.randint(1,1000000000000000))
   graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + file_path + now + "*.jpg " + gif_file_name + ".gif"
 
   os.system(graphicsmagick)
+
+  # Without Overlay
+  # global gif_file_name
+  # gif_file_name = file_path + now + str(random.randint(1,1000000000000000))
+  # graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + file_path + now + "*.jpg " + gif_file_name + ".gif"
+
+  # os.system(graphicsmagick)
 
 # Trigger photobooth workflow
 def start_photobooth():
